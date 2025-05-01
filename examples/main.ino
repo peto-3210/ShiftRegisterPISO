1ce53cc7c1a79c0f06783a3391f4cef4a6a744f1
peto-3210 <petoseges@gmail.com>
2025-05-02 01:36:16 +0200

    changed structure

---
+++ examples/main.ino
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include <PISORegister.h>
+
+#define clkPin 5
+#define ldPin 4
+#define qhPin 0
+#define inputCount 8
+
+PISORegister r1;
+
+void setup() {
+  r1.Init(clkPin, ldPin, qhPin, inputCount, true);
+  r1.SetReadingDelay(100);
+  r1.SetLdClkPulseDelay(5);
+  r1.SetPulseWidth(50);
+}
+
+void loop() {
+  r1.ReadData();
+}
